f2003b1ad380e1efec5d6fd7bc7b617bf4ed54d3
Aicha ZEROUAL <aicha.zeroual01@gmail.com>
2026-05-10 18:36:49 +0100

    entities

---
+++ src/main/java/org/sid/digitalbankingbackend/entities/BankAccount.java
@@ -0,0 +1,27 @@
+package org.sid.digitalbankingbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.digitalbankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE",length = 4)
+@Data @NoArgsConstructor @AllArgsConstructor
+public class BankAccount {
+    @Id
+    private Long id1;
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation> accountOperations;
+}
